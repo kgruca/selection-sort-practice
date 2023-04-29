d00b61765f627adfc7acf3a08d9c37485e897db1
Krzysztof Gruca <kgruca21@gmail.com>
2023-04-28 19:37:52 -0700

    selection sort practice 4.28.23

--- selection_sort.py
+++ selection_sort.py
@@ -1,28 +1,54 @@
-# def findSmallest(arr):
-#     smallest = 0
-#     smallest_value = arr[0]
+# # def findSmallest(arr):
+# #     smallest = 0
+# #     smallest_value = arr[0]
+
+# #     for i in range(1, len(arr)):
+# #         if arr[i] < smallest_value:
+# #             smallest = i
+# #             smallest_value = arr[i]
+
+# #     return smallest
+
+
+# # def selection_sort(arr):
+# #     new_arr = []
+
+# #     for i in range(len(arr)):
+# #         smallest = findSmallest(arr)
+# #         new_arr.append(arr.pop(smallest))
+
+# #     return new_arr
+
+
+# # my_list = [345, 23, 1235, 5667, 6, 83, 664]
+
+# # print(selection_sort(my_list))
+
+
+# def smallest(arr):
+#     smallest_index = 0
+#     smallest_value = arr[smallest_index]
 
 #     for i in range(1, len(arr)):
 #         if arr[i] < smallest_value:
-#             smallest = i
+#             smallest_index = i
 #             smallest_value = arr[i]
 
-#     return smallest
+#     return smallest_index
 
 
 # def selection_sort(arr):
 #     new_arr = []
 
 #     for i in range(len(arr)):
-#         smallest = findSmallest(arr)
-#         new_arr.append(arr.pop(smallest))
+#         new_arr.append(arr.pop(smallest(arr)))
 
 #     return new_arr
 
 
-# my_list = [345, 23, 1235, 5667, 6, 83, 664]
+# newer_list = [4, 1, 66, 4, 2, 50, 717, 34, 18, 91]
 
-# print(selection_sort(my_list))
+# print(selection_sort(newer_list))
 
 
 def smallest(arr):
@@ -41,11 +67,12 @@ def selection_sort(arr):
     new_arr = []
 
     for i in range(len(arr)):
-        new_arr.append(arr.pop(smallest(arr)))
+        smallest_index = smallest(arr)
+        new_arr.append(arr.pop(smallest_index))
 
     return new_arr
 
 
-newer_list = [4, 1, 66, 4, 2, 50, 717, 34, 18, 91]
+practice_arr = [4, 23, 7, 1, 14, 83, 74, 11, 114, 201, 59]
 
-print(selection_sort(newer_list))
+print(selection_sort(practice_arr))
